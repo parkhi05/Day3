1f51a9c8e97b0d637ce55bc8a560c8d55377b011
parkhi05 <38903914+parkhi05@users.noreply.github.com>
2018-05-07 15:20:45 +0530

    Add files via upload

---
+++ Pyramid.java
@@ -0,0 +1,38 @@
+
+package pyramid;
+
+public class Pyramid {
+
+    
+    public static void main(String[] args) {
+        int i,row_count=5,j;
+       for(i=1;i<=row_count;i++)
+       { 
+           System.out.println();
+          int num=1;
+             for(j=1;j<=i;j++)
+           {
+             System.out.print(num);
+             num++;
+           }
+             j++;
+       }
+    }
+}
+           
+        
+        
+        
+        
+        
+        
+        
+        /* for(i=1;i<=5;i++)
+        {  System.out.println("");
+            for(j=1;j<=i;j++)
+            { System.out.print("*");
+        } j++;
+        } */
+       
+        
+    
