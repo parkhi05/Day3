75d1611823f4efd92fa5de8f8b3c4965c5e243eb
parkhi05 <38903914+parkhi05@users.noreply.github.com>
2018-05-07 14:20:24 +0530

    Update Sumofdigits.java

--- Sumofdigits.java
+++ Sumofdigits.java
@@ -1,5 +1,20 @@
-run:
-Enter the no. : 
-5235
-The sum is : 15
-BUILD SUCCESSFUL (total time: 5 seconds)
+package sumofdigits;
+import java.util.*;
+
+public class Sumofdigits {
+    public static void main(String[] args) {
+     Scanner s = new Scanner(System.in);
+     int a,last_digit,sum=0;
+     System.out.println("Enter the no. : ");
+     a=s.nextInt();
+     while(a>0)
+     {
+         last_digit=a%10; //This finds the last digit of a no.
+         sum+=last_digit; //this will add the last digit to a new variable - sum
+         a=a/10; //This will remove the last digit
+       
+     }
+       System.out.println("The sum is : "+sum);
+     }
+    
+}
